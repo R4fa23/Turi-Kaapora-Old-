dc666b986c1eca34b58818d6baee72c96f30b132
Canps <mudiasmattos@gmail.com>
2022-10-07 16:29:44 -0300

    ajuste na fire trap

--- Assets/3. Scripts/Traps/FireTrap.cs
+++ Assets/3. Scripts/Traps/FireTrap.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class FireTrap : MonoBehaviour
 {
     public SOPlayer soPlayer;
+    bool fired;
     
     void OnTriggerStay(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            soPlayer.soPlayerHealth.Burned(3);
+            if(!fired)
+            {
+                soPlayer.soPlayerHealth.Burned(3);
+                fired = true;
+                StartCoroutine(CooldownFire());
+            }
+            
         }
+    }
+
+    IEnumerator CooldownFire()
+    {
+        yield return new WaitForSeconds(0.5f);
+        fired = false;
     }
 }
