2be751b368d85900bf346b0f82284190542e8dc3
Canps <mudiasmattos@gmail.com>
2022-10-11 18:04:46 -0300

    correção player special

--- Assets/3. Scripts/Player/PlayerSpecial.cs
+++ Assets/3. Scripts/Player/PlayerSpecial.cs
@@ -45,10 +45,10 @@ public class PlayerSpecial : MonoBehaviour
     {
         if(other.CompareTag("Enemy"))
         {
-            if(!soPlayer.soPlayerAttack.hitKill) other.GetComponent<EnemyManager>().soEnemy.ChangeLife(-soPlayer.soPlayerAttack.specialDamage);
+            if(!soPlayer.soPlayerAttack.hitKill) other.transform.parent.transform.parent.GetComponent<EnemyManager>().soEnemy.ChangeLife(-soPlayer.soPlayerAttack.specialDamage);
             else other.transform.parent.transform.parent.GetComponent<EnemyManager>().soEnemy.ChangeLife(-1000);
             
-            other.GetComponent<EnemyManager>().soEnemy.Repulsion();
+            other.transform.parent.transform.parent.GetComponent<EnemyManager>().soEnemy.Repulsion();
         }
     }
 
