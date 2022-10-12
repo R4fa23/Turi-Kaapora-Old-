51daf251b3f0dc9adfe59c8d5d8de7c41a5f6e97
Rafael Pedro <r4fa38@gmail.com>
2022-10-12 11:14:25 -0300

    DOTween instalado

--- Assets/BlueFireWall.cs
+++ Assets/BlueFireWall.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class BlueFireWall : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] VisualEffect vfxFire;
+    [SerializeField] float rate;
+    [SerializeField] float wallSize;
+    float xValue;    
+    bool turnOn;
+    bool turnOff;
     void Start()
     {
-        
+        StartFire();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (turnOn)
+        {
+            xValue = vfxFire.GetFloat("Line End");
+            xValue += rate * Time.deltaTime;
+            vfxFire.SetFloat("Line End", xValue);
+            Debug.Log(xValue);
+        }
         
+        if (turnOff)
+        {
+            xValue = vfxFire.GetFloat("Line End");
+            xValue -= rate * Time.deltaTime;
+            vfxFire.SetFloat("Line End", xValue);
+            Debug.Log(xValue);
+        }
+    }
+
+    IEnumerator FireTimerOn()
+    {
+        yield return new WaitUntil(() => xValue >= wallSize);
+        Debug.Log("End");
+        turnOn = false;
+    }
+    IEnumerator FireTimerOff()
+    {
+        yield return new WaitUntil(() => xValue <= 0);
+        Debug.Log("End");
+        turnOff = false;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    public void StartFire()
+    {
+        StartCoroutine(FireTimerOn());
+        turnOn = true;
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+    public void EndFire()
+    {
+        StartCoroutine(FireTimerOff());
+        turnOff = true;        
     }
 }
