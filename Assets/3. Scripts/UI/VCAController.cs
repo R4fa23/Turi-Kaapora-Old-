e6b9b43519659365fcd4e94ed0e6c43a2dc4c3c6
ryanebani <ryanebani@gmail.com>
2022-10-20 17:30:23 -0300

    Pause com Config de Volume

---
+++ Assets/3. Scripts/UI/VCAController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VCAController : MonoBehaviour
+{
+    private FMOD.Studio.VCA VCAMix;
+    public string VCAName;
+    private Slider slider;
+    void Start()
+    {
+        VCAMix = FMODUnity.RuntimeManager.GetVCA("vca:/" + VCAName);
+        slider = GetComponent<Slider>();
+    }
+
+    public void AdjustVolume(float volume)
+    {
+        VCAMix.setVolume(volume);
+    }
+
+    
+}
