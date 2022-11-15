593f35c2f7eb9f9d516fc9ecb1246acdd5e89adc
Canps <mudiasmattos@gmail.com>
2022-11-15 01:59:34 -0300

    levelVfx

--- Assets/LevelVFX.cs
+++ Assets/LevelVFX.cs
@@ -7,12 +7,26 @@ public class LevelVFX : MonoBehaviour
 {
     [SerializeField] VisualEffect[] vfxs;
 
-    public void UpdateVFXLevel(int vfxLevel)
+    public SOPlayer soPlayer;
+
+    public void UpdateVFXLevel()
     {
+        int vfxLevel = soPlayer.level + 1;
+
         foreach (var effect in vfxs)
         {
             effect.SetInt("Level", vfxLevel);
         }
+    }
+
+    private void OnEnable()
+    {
+        soPlayer.LevelUpEvent.AddListener(UpdateVFXLevel);
+    }
+
+    private void OnDisable()
+    {
+        soPlayer.LevelUpEvent.RemoveListener(UpdateVFXLevel);
     }
 
 }
