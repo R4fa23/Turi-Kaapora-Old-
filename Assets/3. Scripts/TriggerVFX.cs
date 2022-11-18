904c056ff5550d78ea1c2c60ea7758c8b0401ee8
Rafael Pedro <r4fael38@gmail.com>
2022-11-18 04:30:07 -0300

    vfx dos ataques 1 e 2

--- Assets/3. Scripts/TriggerVFX.cs
+++ Assets/3. Scripts/TriggerVFX.cs
@@ -1,27 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class TriggerVFX : MonoBehaviour
 {
     public SOPlayer soPlayer;
-    public VisualEffect[] vfx;
+    public VisualEffect[] attacks;
+    public VisualEffect special;
+    public Transform player;
     //public string eventToTrigger;
 
-    public void VFXEvent(string trigger)
+    public void Attack(int vfxName)
     {
-        for (int i = 0; i < vfx.Length; i++)
+        for (int i = 0; i < attacks.Length; i++)
         {
-            if (vfx[i].name == trigger)
+            if (i == vfxName)
             {
-                Debug.Log(vfx[i].name);
-                vfx[i].Reinit();
-                vfx[i].SendEvent("Trigger");
+                attacks[i].SetFloat("Y Angle", player.eulerAngles.y);
+                attacks[i].Reinit();
+                attacks[i].SendEvent("Attack");
             }
         }        
     }
 
+    public void SparksAttack2(int start)
+    {
+        for (int i = 0; i < attacks.Length; i++)
+        {
+            if (i == 1)
+            {
+                if (start == 1) attacks[i].SendEvent("start sparks");
+                else attacks[i].SendEvent("stop sparks");
+            }
+        }
+        
+    }
+    public void Special()
+    {
+        special.Reinit();
+        special.SendEvent("Special");
+    }
     public void StartSpecial()
     {
         soPlayer.soPlayerAttack.SpecialStart();
