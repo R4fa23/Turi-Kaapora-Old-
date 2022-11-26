d55661a8cd423847c777a2ae16e16837d79939cd
Rafael Pedro <r4fael38@gmail.com>
2022-11-26 20:17:58 -0300

    onca na jaula

--- Assets/3. Scripts/Trail/Cage.cs
+++ Assets/3. Scripts/Trail/Cage.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cage : MonoBehaviour
@@ -16,39 +17,50 @@ public class Cage : MonoBehaviour
     public GameObject big;
     public enum animal {Tucano, Macaco, Onca, Jacare};
     public animal animals;
-
     [SerializeField] BoxCollider colliderCage;
 
     [SerializeField] bool update;
 
+    [SerializeField] Animator animalAnimator;
+
+    [SerializeField] Vector3 sizeBig;
+    [SerializeField] Vector3 sizeSmall;
+
+    [SerializeField] Vector3 target;
+    [SerializeField] bool move;
+
+    public float velocity;
+    Transform animalTransform;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        ChangeAnimal();
+    }
 
-        if (update)
+    public void ChangeAnimal()
+    {
+        for (int i = 0; i < animalsArray.Length; i++)
         {
-            for (int i = 0; i < animalsArray.Length; i++)
+            if (animalsArray[i].name == animals.ToString())
             {
-                if (animalsArray[i].name == animals.ToString())
+                if (animalsArray[i].name == "Tucano" || animalsArray[i].name == "Macaco")
                 {
-                    if (animalsArray[i].name == "Tucano" || animalsArray[i].name == "Macaco")
-                    {
-                        small.SetActive(true);
-                        colliderCage.size = small.transform.localScale;
-                        big.SetActive(false);
-                    }
-                    else
-                    {
-                        small.SetActive(false);
-                        big.SetActive(true);
-                        colliderCage.size = big.transform.localScale;
-                    }
-                    animalsArray[i].SetActive(true);
+                    small.SetActive(true);
+                    colliderCage.size = sizeSmall;
+                    animalAnimator = animalsArray[i].GetComponent<Animator>();
+                    big.SetActive(false);
                 }
-                else animalsArray[i].SetActive(false);
+                else
+                {
+                    small.SetActive(false);
+                    big.SetActive(true);
+                    animalAnimator = animalsArray[i].GetComponent<Animator>();
+                    colliderCage.size = sizeBig;
+                }
+                animalsArray[i].SetActive(true);
             }
-            update = false;
+            else animalsArray[i].SetActive(false);
         }
     }
 
@@ -56,28 +68,18 @@ public class Cage : MonoBehaviour
     {
         if (update)
         {
-            for (int i = 0; i < animalsArray.Length; i++)
-            {
-                if (animalsArray[i].name == animals.ToString())
-                {
-                    if (animalsArray[i].name == "Tucano" || animalsArray[i].name == "Macaco")
-                    {
-                        small.SetActive(true);
-                        colliderCage.size = small.transform.localScale;
-                        big.SetActive(false);
-                    }
-                    else
-                    {
-                        small.SetActive(false);
-                        big.SetActive(true);
-                        colliderCage.size = big.transform.localScale;
-                    }
-                    animalsArray[i].SetActive(true);
-                }
-                else animalsArray[i].SetActive(false);                
-            }        
+            ChangeAnimal();
             update = false;
         }
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            animalTransform.position += animalTransform.forward * Time.deltaTime * velocity;
+            //animalTransform.position = Vector3.MoveTowards(animalTransform.position, target, velocity * Time.deltaTime);
+        }        
     }
 
     public void LoseLife()
@@ -97,5 +99,29 @@ public class Cage : MonoBehaviour
         animator.SetTrigger("open");
         soPlayer.soPlayerAttack.EnemyDie(gameObject);
         soTrail.BreakCage();
+        FreeAnimal();
+    }
+
+    public void FreeAnimal()
+    {
+        animalAnimator.SetTrigger("free");
+        StartCoroutine(WatiTransition());
+    }
+
+    IEnumerator WatiTransition()
+    {
+        for (int i = 0; i < animalsArray.Length; i++)
+        {
+            if (animalsArray[i].name == animals.ToString())
+            {
+                animalTransform = animalsArray[i].transform;
+                Debug.Log(animalTransform);              
+                
+            }
+        }
+
+        yield return new WaitWhile(() => animalAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1f);
+        Debug.Log("Walking");        
+        move = true;
     }
 }
